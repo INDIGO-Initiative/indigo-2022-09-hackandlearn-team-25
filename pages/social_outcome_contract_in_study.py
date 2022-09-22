82b174a308adb18dab04633afa3c4ee9aeacaa86
James <james.baster@opendataservices.coop>
2022-09-22 11:22:57 +0100

    Add all report measures

--- pages/social_outcome_contract_in_study.py
+++ pages/social_outcome_contract_in_study.py
@@ -10,9 +10,21 @@ dash.register_page(__name__)
 
 layout = html.Div(children=[
     html.H1(children='Social Outcome contracts in Study'),
-
+    html.H2(children='Changes/Variations'),
     dcc.Graph(
-        id='reports4'
+        id='report1'
+    ),
+    html.H2(children='Costs and resources'),
+    dcc.Graph(
+        id='report2'
+    ),
+    html.H2(children='Ecosystem/System strengthening effects'),
+    dcc.Graph(
+        id='report3'
+    ),
+    html.H2(children='Scalability and Sustainability of SOC'),
+    dcc.Graph(
+        id='report4'
     ),
     dcc.Interval(
         id="load_interval",
@@ -25,57 +37,103 @@ layout = html.Div(children=[
 
 
 @callback(
-    Output(component_id='reports4', component_property='figure'),
+    Output(component_id='report1', component_property='figure'),
+    Input(component_id="load_interval", component_property="n_intervals"),
+)
+def report1(load_interval):
+    data = _get_report_data([
+        ('reports_changes_in_service_delivery_intervention_specific','service delivery - intervention specific'),
+        ('reports_changes_in_person_level_outcomes','person-level outcomes'),
+        ('reports_unintended_effects','unintended effects'),
+        ('reports_auxiliary_resource_or_parallel_reforms_occurring_alongside_soc','Auxiliary resource or parallel reforms'),
+        ('reports_on_acceptability_of_SOC_political_cultural','acceptability of SOC (political/cultural)'),
+        ('reports_implications_for_management_information_systems','implications for management information systems'),
+    ])
+    #print("1" + str(data))
+    df = pandas.DataFrame(data)
+    # See https://community.plotly.com/t/inconsistent-callback-error-updating-scatter-plot/46754/8 for the hack for this terrible try / except
+    try:
+        fig = px.bar(df, x="label", y=["present",'absent'], color_discrete_sequence=['blue','red'])
+    except Exception:
+        fig = px.bar(df, x="label", y=["present", 'absent'], color_discrete_sequence=['blue', 'red'])
+    return fig
+
+@callback(
+    Output(component_id='report2', component_property='figure'),
+    Input(component_id="load_interval", component_property="n_intervals"),
+)
+def report2(load_interval):
+    data = _get_report_data([
+        ('reports_costs_and_resource_implications','Reports on costs and resource implications - especially transaction costs'),
+    ])
+    #print("2" + str(data))
+    df = pandas.DataFrame(data)
+    try:
+        fig = px.bar(df, x="label", y=["present",'absent'], color_discrete_sequence=['blue','red'])
+    except Exception:
+        fig = px.bar(df, x="label", y=["present", 'absent'], color_discrete_sequence=['blue', 'red'])
+    return fig
+
+
+@callback(
+    Output(component_id='report3', component_property='figure'),
+    Input(component_id="load_interval", component_property="n_intervals"),
+)
+def report3(load_interval):
+    data = _get_report_data([
+        ('reports_ecosystem_or_system_strengthening_effects','Reports on ecosystem or system strengthening effects - i.e. implications beyond the SOC parties'),
+    ])
+    #print("3" + str(data))
+    df = pandas.DataFrame(data)
+    try:
+        fig = px.bar(df, x="label", y=["present",'absent'], color_discrete_sequence=['blue','red'])
+    except Exception:
+        fig = px.bar(df, x="label", y=["present", 'absent'], color_discrete_sequence=['blue', 'red'])
+    return fig
+
+
+@callback(
+    Output(component_id='report4', component_property='figure'),
     Input(component_id="load_interval", component_property="n_intervals"),
 )
 def report4(load_interval):
-    connection = sqlite3.connect('data/research-projects-database.sqlite')
-    with closing(connection.cursor()) as cursor:
-
-        cursor.execute(
-            "SELECT count(*) AS c FROM social_outcomes_contract WHERE reports_scalability = 'present' OR reports_scalability = 'Present'",
-            []
-        )
-        reports_scalability_present = cursor.fetchone()[0]
-
-        cursor.execute(
-            "SELECT count(*) AS c FROM social_outcomes_contract WHERE reports_long_term_sustainment_and_legacy_effects = 'present' OR reports_long_term_sustainment_and_legacy_effects = 'Present'",
-            []
-        )
-        reports_long_term_sustainment_and_legacy_effects_present = cursor.fetchone()[0]
-
-        cursor.execute(
-            "SELECT count(*) AS c FROM social_outcomes_contract WHERE NOT( reports_scalability = 'present' OR reports_scalability = 'Present')",
-            []
-        )
-        reports_scalability_absent = cursor.fetchone()[0]
-
-        cursor.execute(
-            "SELECT count(*) AS c FROM social_outcomes_contract WHERE NOT (reports_long_term_sustainment_and_legacy_effects = 'present' OR reports_long_term_sustainment_and_legacy_effects = 'Present')",
-            []
-        )
-        reports_long_term_sustainment_and_legacy_effects_absent = cursor.fetchone()[0]
-
-    data = {
-        'label': [
-            'reports_scalability',
-            'reports_long_term_sustainment_and_legacy_effects'
-        ],
-        'present': [
-            reports_scalability_present,
-            reports_long_term_sustainment_and_legacy_effects_present
-        ],
-        'absent': [
-            reports_scalability_absent,
-            reports_long_term_sustainment_and_legacy_effects_absent
-        ]
-    }
-    #print(data)
+    data = _get_report_data([
+        ('reports_scalability','Reports on scale/scalability of the SOC approach'),
+        ('reports_long_term_sustainment_and_legacy_effects','Reports on long-term sustainment and \'post-SOC\' legacy effects'),
+    ])
+    #print("4" + str(data))
     df = pandas.DataFrame(data)
-    #print(df)
-    fig = px.bar(df, x="label", y=["present",'absent'], color_discrete_sequence=['blue','red'])
-    connection.close()
+    try:
+        fig = px.bar(df, x="label", y=["present",'absent'], color_discrete_sequence=['blue','red'])
+    except Exception:
+        fig = px.bar(df, x="label", y=["present", 'absent'], color_discrete_sequence=['blue', 'red'])
     return fig
 
 
 
+def _get_report_data(field_and_label_list):
+    connection = sqlite3.connect('data/research-projects-database.sqlite')
+    data = {
+        'label': [],
+        'present': [],
+        'absent': []
+    }
+    with closing(connection.cursor()) as cursor:
+        for field, label in field_and_label_list:
+
+            data['label'].append(label)
+
+            cursor.execute(
+                "SELECT count(*) AS c FROM social_outcomes_contract WHERE "+field+" = 'present' OR "+field+" = 'Present'",
+                []
+            )
+            data['present'].append(cursor.fetchone()[0])
+
+            cursor.execute(
+                "SELECT count(*) AS c FROM social_outcomes_contract WHERE  NOT("+field+" = 'present' OR "+field+" = 'Present')",
+                []
+            )
+            data['absent'].append(cursor.fetchone()[0])
+
+    connection.close()
+    return data
