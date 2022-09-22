17f22999ae470944f4884c4875977a1ec066629b
James <james.baster@opendataservices.coop>
2022-09-22 11:33:33 +0100

    make home page appear at actual home

--- pages/home.py
+++ pages/home.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import pandas as pd
 import sqlite3
 
-dash.register_page(__name__)
+dash.register_page(__name__, path='/')
 
 layout = html.Div(children=[
     html.H1(children='Data Dashboard'),
